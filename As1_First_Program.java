f6c9ed9dc3ea886d2ccfef51927338eb7085fde4
uthra <uthra182505@gmail.com>
2024-02-29 14:02:33 +0800

    My First Assignment

---
+++ As1_First_Program.java
@@ -0,0 +1,12 @@
+package assignment;
+
+public class As1_First_Program //Identifier
+{
+
+	public static void main(String[] args) // main method
+	{
+		System.out.println("This is my First program");
+
+	}
+
+}
